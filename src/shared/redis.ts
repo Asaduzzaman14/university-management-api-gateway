22ef84df790e5d4d67c037116f2b3ca9c30f65e2
Md Asaduzaman <asadasad9282@gmail.com>
2023-09-15 23:08:43 +0600

    redis connect

--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -2,9 +2,21 @@ import { createClient } from 'redis';
 import logger from './logger';
 
 let redisClient = createClient({
-  url: 'https://localhost:6379'
+  url: 'redis://localhost:6379'
 });
 
 redisClient.on('error', (err) => {
   logger.error('Redis Error', err);
 });
+
+redisClient.on('error', (err) => {
+  logger.info('Redis Conected');
+});
+
+const connect = async (): Promise<void> => {
+  await redisClient.connect();
+};
+
+export const RedisClient = {
+  connect
+};
